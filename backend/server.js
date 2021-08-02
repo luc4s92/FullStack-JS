7c52e7f27523f9afb326680980ec9e39746e57f4
Lucas <lucas.acdc92@gmail.com>
2021-08-02 16:49:11 -0300

    Create the connection with de database

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,44 @@
-import express from 'express';
+const express = require('express')
+//const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
+//MySql
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'lucas',
+    password: '1234',
+    database: 'lucas'
+})
+
+app.get('', (req, res) =>{
+
+    pool.getConnection((err, connection) =>{
+        if(err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        connection.query('SELECT * from budgets', (err, rows) =>{
+            connection.release()
+
+            if(!err){
+                res.send(rows)
+            }else{
+                console.log(err)
+            }
+        })
+
+    })
+})
+
+//Listen on enviroment port or 5000
+app.listen(port, () =>{
+    console.log(`Listen on port ${port}`)
+})
+
